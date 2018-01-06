48d381ebefd6e3599257d70c0488780341e9ac54
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2018-01-06 21:39:15 +0300

    add github key

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,4 +3,11 @@
 # By default Travis checks out commit, and maven-release-plugin wants to know branch name
 git checkout $TRAVIS_BRANCH
 
+# Decrypt GitHub SSH key
+openssl aes-256-cbc -K $encrypted_e577282bf6cb_key -iv $encrypted_e577282bf6cb_iv -in .travis/github_deploy.enc -out .travis/github_deploy -d
+chmod 600 .travis/github_deploy
+# Add
+eval `ssh-agent -s`
+ssh-add .travis/github_deploy
+
 mvn -B release:prepare release:perform
