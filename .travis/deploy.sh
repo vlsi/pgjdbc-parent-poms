14bd210632a7515b57ffb9bed8cef56a52b8ce01
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2018-01-06 22:19:07 +0300

    add gpg key

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,11 +3,20 @@
 # By default Travis checks out commit, and maven-release-plugin wants to know branch name
 git checkout $TRAVIS_BRANCH
 
+cd .travis
+
+# GPG is required for artifact signing
+openssl aes-256-cbc -K $encrypted_e577282bf6cb_key -iv $encrypted_e577282bf6cb_iv -in secrets.tar.enc -out secrets.tar -d
+tar xvf secrets.tar
+
+gpg --import gpg-secret.key
+gpg --import-ownertrust gpg-ownertrust && echo "imported ownertrust"
+
 # Decrypt GitHub SSH key
-openssl aes-256-cbc -K $encrypted_e577282bf6cb_key -iv $encrypted_e577282bf6cb_iv -in .travis/github_deploy.enc -out .travis/github_deploy -d
-chmod 600 .travis/github_deploy
-# Add
+chmod 600 github_deploy
 eval `ssh-agent -s`
-ssh-add .travis/github_deploy
+ssh-add ./github_deploy
+
+cd ..
 
 mvn -B -DpushChanges=false release:prepare release:perform
